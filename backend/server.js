d5fb07ad0ccb9f832752e28fe3e4723f7abc3718
Ivin1998 <a.ivinaustan@gmail.com>
2024-03-13 00:29:43 +0530

    updated the Cross-Origin Resource Sharing (CORS) issue

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://iv-chat.onrender.com",
     methods: ["GET", "POST"],
   },
 });
